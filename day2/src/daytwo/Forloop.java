483471cbcbb02ba1cbd4113283e75362805b9999
veena l c <veenaammu35@gmail.com>
2023-03-04 10:35:25 +0530

    hii

---
+++ day2/src/daytwo/Forloop.java
@@ -0,0 +1,11 @@
+package daytwo;
+
+public class Forloop {
+	public static void main(String[] args) {
+		for(int i=1;i<=1000;i++){
+			System.out.println("value od i:");
+			System.out.println(i);
+		}
+	}
+
+}
