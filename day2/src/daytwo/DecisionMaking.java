ab83aa621e5d2dd119adb06f03c2b8529993ac2d
veena l c <veenaammu35@gmail.com>
2023-03-04 10:28:07 +0530

    hi

---
+++ day2/src/daytwo/DecisionMaking.java
@@ -0,0 +1,21 @@
+package daytwo;
+
+public class DecisionMaking {
+	public static void main(String[] args) {
+		int x=8,y=7;
+		int a=10;
+		int b=11;
+		
+		if(x>=y){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+		if(!(a>b)||(a==b)){      //false||false
+			System.out.println("condition id true");
+		}else{
+			System.out.println("condition is false");
+		}
+	}
+
+}
