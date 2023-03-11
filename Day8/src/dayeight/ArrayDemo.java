f261c71322b831df35402c77767be4793c4fbee9
veena l c <veenaammu35@gmail.com>
2023-03-11 11:41:18 +0530

    commit

---
+++ Day8/src/dayeight/ArrayDemo.java
@@ -0,0 +1,16 @@
+package dayeight;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		String a[]=new String [2];
+		a[0]="veena";
+		a[1]="ammu";
+//		a[2]=56;
+//		a[3]=78;
+//		a[4]=89;
+		for(int i=0;i<a.length;i++){
+			System.out.println(i);
+		}
+	}
+
+}
