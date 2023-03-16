1727fcdd0adb274fcdffeb572ac5c14a11fbfead
veena l c <veenaammu35@gmail.com>
2023-03-16 21:21:40 +0530

    commit

---
+++ Day9/src/daynine/Variables.java
@@ -0,0 +1,14 @@
+package daynine;
+
+public class Variables {
+	int s=45;
+	static int g=25;
+	public static void main(String[] args) {
+		int r=23;
+		System.out.println(r);
+		System.out.println(g);
+		Variables obj=new Variables();
+		System.out.println(obj.s);
+	}
+
+}
