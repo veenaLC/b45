b60e2049add77b5141e79cccbcb5b310948b8d88
veena l c <veenaammu35@gmail.com>
2023-05-07 19:46:38 +0530

    JPAOneToOneUni

---
+++ One_to_one_uni/src/com/cg/onetooneuni/Client.java
@@ -0,0 +1,34 @@
+package com.cg.onetooneuni;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		Student student = new Student();
+		student.setName("Veena L C");
+		Address homeAddress = new Address();
+		homeAddress.setStreet("Basham cicle");
+		homeAddress.setCity("Bangalore");
+		homeAddress.setState("Karnakta");
+		homeAddress.setZipCode("577533");
+		
+		//inject address into student
+		student.setAddress(homeAddress);
+		
+		//persist only student, no need to persist Address explicitly
+		em.persist(student);
+		em.getTransaction().commit();
+		
+		System.out.println("Added one student with address to database.");
+		em.close();
+		factory.close();
+	}
+}
+
